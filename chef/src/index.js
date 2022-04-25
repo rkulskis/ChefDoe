5e356a955573150452bac3154cab645a1cd43961
ryanv249 <47526648+ryanv249@users.noreply.github.com>
2022-04-25 17:50:23 -0400

    ultra super basic react thing happening now

--- chef/src/index.js
+++ chef/src/index.js
@@ -1,9 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import styling from './index.css';
+
+
+
+class Mover extends React.Component{
+	logClick(){
+		
+	}
+	render(){
+		return <div class = "mainButton flexer" onClick = {() => console.log('click')}>Cook</div>
+	}
+}
+
+class MainPage extends React.Component{
+	render(){
+		return(
+			<>
+	      	<div id = "mainTab" class = "flexer">
+	      		<Mover />
+{/*	      		<div class="mainButton flexer">
+	        	Cook
+	        	<div id = "cookCover" class = "flexer"></div>*/}
+	     		<div class="mainButton flexer">Recipes</div>
+	      		<div class="mainButton flexer">Account</div>
+	    	</div>
+
+	    	<div id = "cookTab" class = "subTab flexer">
+	      		<div class="subButton cook flexer">Browse Recipes</div>
+	     		<div class="subButton cook flexer">Random Recipe</div>
+	    	</div>
+
+	    	<div id = "manageTab" class = "subTab flexer">
+	      		<div class="subButton manage flexer">Manage Recipes</div>
+	     		<div class="subButton manage flexer">Add New Recipe</div>
+	    	</div>
+
+	    	<div id = "accTab" class = "subTab flexer">
+	      		<div class="subButton acc flexer">Update Address</div>
+	      		<div class="subButton acc flexer">Update Payment</div>
+	    	</div>
+
+	    	</>
+    	);
+	}
+}
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render();
+root.render(<MainPage />);
 
