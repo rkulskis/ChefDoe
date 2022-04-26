85dc882cf2595d5f60f85ec049c1164bb12fdda6
ryanv249 <47526648+ryanv249@users.noreply.github.com>
2022-04-25 20:10:19 -0400

    yet another huge refactor, hoping this setup is final

---
+++ chef/src/App.js
@@ -0,0 +1,16 @@
+import './index.css';
+
+function App() {
+  return (
+    <Navbar></Navbar>
+  );
+}
+
+function Navbar(){
+  return (
+    <div className = "mainTab flexer">
+    </div>
+  );
+}
+
+export default App;
--- chef/src/index.js
+++ chef/src/index.js
@@ -1,70 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
-const colors = ['red', 'blue', 'green', 'yellow', 'black'];
-
-class Mover extends React.Component{
-	constructor(props){
-		super(props);
-		this.state = {
-			color: 'red'
-		};
-	}
-
-	getRandomColor(){
-		const diffColors = colors.filter(color => color !== this.state.color);
-		const randomColorIndex = Math.floor(Math.random() * diffColors.length);
-		return diffColors[randomColorIndex];
-	}
-
-	handleClick(){
-		this.setState(({color}) => ({color: this.getRandomColor()}));
-	}
-
-	render(){
-		return (
-		<div class = "mainButton flexer" style = {{backgroundColor: this.state.color}} 
-		onClick = {() => this.handleClick()}>Cook</div>
-		);
-	}
-}
-
-class MainPage extends React.Component{
-	render(){
-		return(
-			<>
-	      	<div id = "mainTab" class = "flexer">
-	      		<Mover />
-{/*	      		<div class="mainButton flexer">
-	        	Cook
-	        	<div id = "cookCover" class = "flexer"></div>*/}
-	     		<div class="mainButton flexer">Recipes</div>
-	      		<div class="mainButton flexer">Account</div>
-	    	</div>
-
-	    	<div id = "cookTab" class = "subTab flexer">
-	      		<div class="subButton cook flexer">Browse Recipes</div>
-	     		<div class="subButton cook flexer">Random Recipe</div>
-	    	</div>
-
-	    	<div id = "manageTab" class = "subTab flexer">
-	      		<div class="subButton manage flexer">Manage Recipes</div>
-	     		<div class="subButton manage flexer">Add New Recipe</div>
-	    	</div>
-
-	    	<div id = "accTab" class = "subTab flexer">
-	      		<div class="subButton acc flexer">Update Address</div>
-	      		<div class="subButton acc flexer">Update Payment</div>
-	    	</div>
-
-	    	</>
-    	);
-	}
-}
-
-
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<MainPage />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
